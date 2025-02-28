ccf8554b05bf02f38a802e0d0e68bcd84fa962ca
raimonvibe <robertjanstefan@gmail.com>
2025-02-28 21:16:30 +0100

    Delete config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    prefix: "!", // Example prefix, not used with slash commands but could be added
-    defaultCooldown: 3, // seconds for command cooldowns (if implementing)
-    // More complex configurations could be added here later
-  };
